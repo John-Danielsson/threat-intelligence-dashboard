d50fa7795000284f2f4b956df53874d16ecd0ea2
John Danielsson <johnaugustd@gmail.com>
2026-01-21 13:26:42 -0800

    added recent pulses function

--- utils.py
+++ utils.py
@@ -1,20 +1,23 @@
+from datetime import datetime, timedelta
+import pandas as pd
 from OTXv2 import OTXv2
 from dotenv import load_dotenv
 import os
-import pandas as pd
+
 
 load_dotenv()
 OTX_API_KEY = os.getenv("OTX_API_KEY")
-otx = OTXv2(OTX_API_KEY)
+OTX = OTXv2(OTX_API_KEY)
+
 
 def get_pulses(count=10):
-    pulses = otx.get_my_pulses()[:count]  # Limit for demo
+    pulses = OTX.get_my_pulses()[:count]  # Limit for demo
     df = pd.DataFrame(pulses)
     return df
 
 def search_ioc(ioc_type, ioc_value):
     # ioc_type: 'IPv4', 'domain', 'FileHash-MD5', etc.
-    details = otx.get_indicator_details_full(ioc_type, ioc_value)
+    details = OTX.get_indicator_details_full(ioc_type, ioc_value)
     # Flatten relevant sections (e.g., pulse_info, malware)
     results = {
         'pulses': details.get('pulse_info', {}).get('pulses', []),
@@ -22,3 +25,16 @@ def search_ioc(ioc_type, ioc_value):
         'reputation': details.get('reputation', {})
     }
     return results
+
+def get_recent_pulses(since_days=7):
+    """Fetch pulses modified in the last X days."""
+    modified_since = (datetime.now() - timedelta(days=since_days)).strftime('%Y-%m-%dT%H:%M:%S')
+    # OTX.get_all_indicators()
+    pulses = OTX.get_all_indicators(modified_since=modified_since, limit=50)  # Adjust limit as needed
+    if not pulses:
+        return pd.DataFrame()  # Empty DF if no recent activity
+    df = pd.DataFrame(pulses)
+    df.sort_values(by="created", ascending=False, inplace=True)
+    # print(df.columns)
+    # ['id', 'indicator', 'type', 'created', 'content', 'title', 'description', 'expiration', 'is_active', 'role']
+    return df
